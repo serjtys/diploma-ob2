8c8c0b974d2233708c01b219097eea0863bdc6ef
serj <sergejtustin601@gmail.com>
2025-10-31 17:09:44 +0300

    fix nickname

--- users/models.py
+++ users/models.py
@@ -17,26 +17,48 @@ class CustomUserManager(BaseUserManager):
         return self.create_user(phone_number, password, **extra_fields)
 
 
+# users/models.py
 class CustomUser(AbstractUser):
     phone_number = models.CharField(max_length=15, unique=True)
-    nickname = models.CharField(max_length=50, unique=True)
+    nickname = models.CharField(max_length=50, unique=True, blank=False)  # Убрали blank=True
     bio = models.TextField(max_length=500, blank=True)
-    avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
     website = models.URLField(blank=True)
 
     username = None
     email = models.EmailField(blank=True, null=True)
 
-    USERNAME_FIELD = "phone_number"
-    REQUIRED_FIELDS = ["nickname"]
-
-    objects = CustomUserManager()
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = []  # Убрали nickname из обязательных
 
     def __str__(self):
-        return self.nickname
+        return self.nickname if self.nickname else f"user_{self.id}"
 
     def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
+        # Генерируем nickname если его нет
+        if not self.nickname or self.nickname.strip() == "":
+            # Сначала сохраняем чтобы получить id
+            if self.pk is None:
+                super().save(*args, **kwargs)
+
+            # Генерируем уникальный nickname
+            base_nickname = f"user_{self.id}"
+            nickname = base_nickname
+            counter = 1
+
+            # Проверяем уникальность
+            while CustomUser.objects.filter(nickname=nickname).exclude(pk=self.pk).exists():
+                nickname = f"{base_nickname}_{counter}"
+                counter += 1
+
+            self.nickname = nickname
+            # Сохраняем снова если это было первое сохранение
+            if self.pk:
+                super().save(update_fields=['nickname'])
+            else:
+                super().save(*args, **kwargs)
+        else:
+            super().save(*args, **kwargs)
 
     def get_display_name(self):
         return self.nickname
